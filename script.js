9fc2b1b111ba4324f1664962f5cc3e8f7104c929
Manjula1999 <144819647+Manjula1999@users.noreply.github.com>
2024-10-07 21:55:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
+// This file is not to be modified. Please ignore this.
+// We will understand all of this later in the course.
+// DO NOT MODIFY THIS FILE
 
-function hasKey(key) {
-  //   write your code here
-}
+const express = require('express');
+const path = require('path');
 
-// Do not change the code below
-const key = prompt("Enter Key.");
-alert(hasKey(key));
+const app = express();
+
+app.use(express.static(__dirname))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/main.html'));
+});
+//your code here
+app.post('/add', (req, res) => {
+  const {a,b} = req.body;
+  res.status(200).send(a+b);
+  // res.sendFile(path.join(__dirname + '/main.html'));
+});
+module.exports = app;
